85ee2d89e01cfbebc38d455d0cbcc292f8b0cc10
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-11-26 23:44:41 +0530

    stack push & pull methods created

--- stack impl/src/Stack.java
+++ stack impl/src/Stack.java
@@ -1,2 +1,33 @@
 public class Stack {
+    int[] stackArr;
+    int maxSize;
+    int top;
+
+    // overloaded constructor
+    public Stack(int size){
+        this.maxSize = size;
+        this.top = -1;
+        stackArr = new int[maxSize];
+    }
+
+    // push method
+    public void push(int value){
+        if(top == maxSize -1){ // check whether stack is full
+            System.out.println("Error : Stack is full");
+        }
+        else{
+            top++;
+            stackArr[top] = value;
+            System.out.println("Inserted "+value);
+        }
+    }
+
+    // pop method
+
+    public int pop(){
+        int value = stackArr[top];
+        top--;
+
+        return value;
+    }
 }

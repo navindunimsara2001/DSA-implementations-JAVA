44ce73925af8ca1fe197a644aed0c96165d91d59
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-11-27 14:45:51 +0530

    stack implement using linked list started

---
+++ stack impl using list/src/stack/impl/Stack.java
@@ -0,0 +1,12 @@
+package stack.impl;
+
+public class Stack {
+    Node head;
+
+    class node{
+        int value;
+        Node next;
+    }
+
+
+}
---
+++ stack impl using list/src/stack/impl/StackApp.java
@@ -0,0 +1,4 @@
+package stack.impl;
+
+public class StackApp {
+}

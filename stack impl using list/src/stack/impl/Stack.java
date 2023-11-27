c5f39aa7f35e160b8f21a3c4ece612345cced915
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-11-27 15:38:32 +0530

    stack implement using linked list complete

--- stack impl using list/src/stack/impl/Stack.java
+++ stack impl using list/src/stack/impl/Stack.java
@@ -1,12 +1,42 @@
 package stack.impl;
 
 public class Stack {
-    Node head;
+    Node topNode;
 
-    class node{
+    class Node{
         int value;
-        Node next;
+        Node next; // this will hold previously entered Node reference
     }
 
+    // constructor
+    public Stack(){
+        topNode = null;
+    }
 
+    // push method : add value
+    public void push(int value){
+        // create new node temp
+        Node temp = new Node();
+        // assign values to temp object
+        temp.value = value;
+        // assign next Node reference(null) to temp
+        temp.next = topNode;   // in here topNode = null
+        // update topNode reference
+        topNode = temp;    // in here topNode has values
+        System.out.println("Inserted "+value);
+    }
+
+    // pop method
+    public int pop(){
+        if(topNode == null) { // check whether stack is empty
+            System.out.println("Error : Stack is empty");
+            return -1;
+        }
+        else{
+            int value = topNode.value;
+            topNode = topNode.next;  // again topNode reference set to previously inserted node
+            System.out.println("Deleted : "+value);
+            return value;
+        }
+    }
 }
--- stack impl using list/src/stack/impl/StackApp.java
+++ stack impl using list/src/stack/impl/StackApp.java
@@ -1,4 +1,22 @@
 package stack.impl;
 
 public class StackApp {
+    public static void main(String[] args) {
+        // create Stack object
+        Stack stack = new Stack();
+        // insert values to stack
+        stack.push(7);
+        stack.push(8);
+        stack.push(9);
+        stack.push(10);
+        stack.push(11);
+
+        // delete value from stack
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();  // this will generate an error
+    }
 }

fd01f599d9e951c610f51f9b3198d8270588355f
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-12-05 20:57:20 +0530

    LinkList class deleteFirst function created

--- linked list impl/src/LinkList.java
+++ linked list impl/src/LinkList.java
@@ -17,10 +17,18 @@ public class LinkList {
             current = current.next;
         }
     }
+
     // insert first method
     public void insertFirst(int data){
-        Link nLink = new Link(data);
+        Link nLink = new Link(data);  // create Link
         nLink.next = first;  // connect previous first Link as next Link
         first = nLink;   // update first link
+    }
+
+    // delete first method
+    public Link deleteFirst(){
+        Link temp = first;  // for return purpose
+        first = first.next;  // first Link change to next Link of current first Link
+        return temp;
     }
 }

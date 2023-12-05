0ded1144091c2d032a7876604ba21c2adcf16e72
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-12-05 17:37:55 +0530

    Link class defined

--- linked list impl/src/Link.java
+++ linked list impl/src/Link.java
@@ -1,2 +1,16 @@
 public class Link {
+    // Link class attributes are public
+    public int iData;  // hold data
+    public Link next; // hold next reference
+
+    // constructor
+    public Link(int data){
+        iData = data;
+        next = null;   // immediately Link is floating
+    }
+
+    // display method
+    public void displayLink(){
+        System.out.println(iData);
+    }
 }

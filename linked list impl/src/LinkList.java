d7fbb71591a868000e2342b13b9fd10511d3607e
navindunimsara2001 <141642402+navindunimsara2001@users.noreply.github.com>
2023-12-05 20:46:40 +0530

    LinkList class displayList & insertList functions created

--- linked list impl/src/LinkList.java
+++ linked list impl/src/LinkList.java
@@ -1,2 +1,26 @@
 public class LinkList {
+    private Link first;
+    public LinkList(){
+        first = null;
+    }
+
+    // check empty
+    public boolean isEmpty(){
+        return (first == null);
+    }
+
+    // display list
+    public void displayList(){
+        Link current = first;
+        while(current != null){
+            current.displayLink();
+            current = current.next;
+        }
+    }
+    // insert first method
+    public void insertFirst(int data){
+        Link nLink = new Link(data);
+        nLink.next = first;  // connect previous first Link as next Link
+        first = nLink;   // update first link
+    }
 }
--- linked list impl/src/LinkListApp.java
+++ linked list impl/src/LinkListApp.java
@@ -1,2 +1,10 @@
 public class LinkListApp {
+    public static void main(String[] args) {
+        Link l1 = new Link(25);
+        Link l2 = new Link(10);
+        Link l3 = new Link(70);
+
+        l1.next = l2;
+        l2.next = l3;
+    }
 }
